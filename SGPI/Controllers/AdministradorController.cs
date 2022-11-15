ecf06456557b82c61f96789ed3d83ddf3629668f
MAURO <MAURO@DESKTOP-U9CTTDE>
2022-11-14 20:40:50 -0500

    Agregar archivos de proyecto.

---
+++ SGPI/Controllers/AdministradorController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGPI.Models;
+using System.Linq;
+
+
+namespace SGPI.Controllers
+{
+    public class AdministradorController : Controller
+    {
+
+        SGPIDBContext context= new SGPIDBContext();
+
+        [HttpPost]
+        public IActionResult Login(Usuario user)
+        {
+
+            var usuario = context.Usuarios
+                .Where(consulta => consulta.NumeroDocumento == user.NumeroDocumento
+                && consulta.Password == user.Password).FirstOrDefault();
+
+            if (usuario != null)
+            {
+                if(usuario.IdRol == 1){ 
+                    return Redirect("Administrador/MenuAdmBuscar");
+                }
+                else if(usuario.IdRol == 2){
+                    return Redirect("Coordinador/MenuCoordinadorBuscar");
+                }
+                else if (usuario.IdRol == 3){
+                    return Redirect("Estudiante/ActualizarEstudiante/?IdUsuario="+usuario.IdUsuario);
+                }
+                else{
+                
+                }
+            }
+            else{ }
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult OlvidarContrasena()
+        {
+            return View();
+        }
+
+        
+
+        public IActionResult MenuRegistro()
+        {
+            ViewBag.genero = context.Generos.ToList();
+            ViewBag.rol = context.Rols.ToList();
+            ViewBag.documento = context.Documentos.ToList();
+            ViewBag.programa = context.Programas.ToList();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult MenuRegistro(Usuario usuario)
+        {
+            context.Usuarios.Add(usuario);
+            context.SaveChanges();
+
+            ViewBag.genero = context.Generos.ToList();
+            ViewBag.rol = context.Rols.ToList();
+            ViewBag.documento = context.Documentos.ToList();
+            ViewBag.programa = context.Programas.ToList();
+            return View();
+        }
+
+        public IActionResult MenuAdmBuscar()
+        {
+            ViewBag.documento = context.Documentos.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MenuAdmBuscar(Usuario usuario)
+        {
+            var us = context.Usuarios
+                .Where(u => u.NumeroDocumento == usuario.NumeroDocumento
+                && u.IdDoc == usuario.IdDoc).FirstOrDefault();
+
+            if (us != null)
+            {
+
+                ViewBag.documento = context.Documentos.ToList();
+                return View(us);
+            }
+            else
+            {
+                ViewBag.documento = context.Documentos.ToList();
+                return View();
+            }
+        }
+
+        public IActionResult MenuAdmModificar(int ? IdUsuario)
+        {
+            Usuario usuario = context.Usuarios.Find(IdUsuario);
+            if (usuario != null)
+            {
+                ViewBag.genero = context.Generos.ToList();
+                ViewBag.rol = context.Rols.ToList();
+                ViewBag.documento = context.Documentos.ToList();
+                ViewBag.programa = context.Programas.ToList();
+                return View(usuario);
+            }
+            else
+                return Redirect("Administrador/MenuAdmBuscar"); 
+        }
+
+
+        [HttpPost]
+        public IActionResult MenuAdmModificar(Usuario user)
+        {
+            context.Update(user);
+            context.SaveChanges();
+
+            ViewBag.genero = context.Generos.ToList();
+            ViewBag.rol = context.Rols.ToList();
+            ViewBag.documento = context.Documentos.ToList();
+            ViewBag.programa = context.Programas.ToList();
+            return Redirect("MenuAdmBuscar");
+        }
+        public IActionResult Delete(Usuario usuario)
+        {
+            context.Remove(usuario);
+            context.SaveChanges();
+            return Redirect("MenuAdmBuscar");
+        }
+        public IActionResult Reportes()
+        {
+            return View();
+        }
+    }
+}
